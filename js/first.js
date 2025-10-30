15f1b3c030b705a762700ad7a9b7a767d78c4e51
LiShaojun1126 <lshaojun1126@gmail.com>
2025-10-30 14:50:27 -0400

    finished lab05

---
+++ js/first.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const name = prompt("Insert your name!");
+const age = Number(prompt("Insert you age"));
+const major = prompt("Insert your major!");
+
+document.write(`name is ${name}, age is ${age}, major is ${major}`);
