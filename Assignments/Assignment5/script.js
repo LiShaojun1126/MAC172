21ebfd1321895b0a77f425b897f10ef6043d0939
LiShaojun1126 <lshaojun1126@gmail.com>
2025-11-11 16:41:01 -0500

    implemented assignment 5

--- Assignments/Assignment5/script.js
+++ Assignments/Assignment5/script.js
@@ -1,22 +1,24 @@
 "use strict";
 // script.js
-const users = {}; // Object to store users
+let users = {}; // Object to store users
 // Sign Up function
 function signup() {
     const name = document.getElementById("signupName").value.trim();
     const email = document.getElementById("signupEmail").value.trim();
     const username = document.getElementById("signupUsername").value.trim();
     const password = document.getElementById("signupPassword").value;
+    const currentUsers = JSON.parse(localStorage.getItem("users"));
     if (!name || !email || !username || !password) {
         showMessage("Please fill in all fields.");
         return;
     }
-    if (users[username]) {
+    if (currentUsers[username]) {
         showMessage("Username already exists.");
         return;
     }
     // Create new user
     users[username] = { name, email, password };
+    localStorage.setItem("users", JSON.stringify(users));
     showMessage("User registered successfully!");
     console.log(users);
 }
@@ -24,6 +26,9 @@ function signup() {
 function login() {
     const username = document.getElementById("loginUsername").value.trim();
     const password = document.getElementById("loginPassword").value;
+    const usersString = localStorage.getItem("users");
+
+    users = JSON.parse(usersString);
     if (users[username] && users[username].password === password) {
         showMessage(`Welcome back, ${users[username].name}!`);
     } else {
